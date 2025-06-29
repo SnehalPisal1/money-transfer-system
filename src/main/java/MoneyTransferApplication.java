5b8b8f2728bfdbd23e6cda073d46616f5079bb54
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-06-29 11:59:39 +0200

    implement

---
+++ src/main/java/MoneyTransferApplication.java
@@ -0,0 +1,12 @@
+/*
+
+3. Money Transfer Endpoint
+
+Design a RESTful API endpoint to transfer money between two accounts.
+
+Ensure atomicity and data consistency.
+
+Handle edge cases (e.g., insufficient funds, invalid accounts).
+
+Discuss how you would prevent race conditions in a concurrent environment.
+ */
