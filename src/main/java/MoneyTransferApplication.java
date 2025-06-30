19fe9dd2a9a4ce2ae8ea433e6a00173bb7c84d51
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-06-30 11:10:53 +0200

    implement system

--- src/main/java/MoneyTransferApplication.java
+++ src/main/java/MoneyTransferApplication.java
@@ -10,3 +10,13 @@ Handle edge cases (e.g., insufficient funds, invalid accounts).
 
 Discuss how you would prevent race conditions in a concurrent environment.
  */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoneyTransferApplication {
+
+}
