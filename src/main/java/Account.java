0e27fa95b98240dc3300c48f1e238603d03ce918
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-07-03 17:56:28 +0200

    Add Account entity

---
+++ src/main/java/Account.java
@@ -0,0 +1,26 @@
+public class Account {
+
+    private long accountId;
+    private double balance;
+
+    public Account(long accountId, double balance) {
+        this.accountId = accountId;
+        this.balance = balance;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
